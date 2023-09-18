54d5d464605cdfe2b77f9fb673ebc790f90b6ac8
Andrey <98714688+Dreymeister@users.noreply.github.com>
2023-09-18 16:39:33 -0400

    Added CSV, JSON, & Plain Functions

---
+++ lib/readPlain.php
@@ -0,0 +1,24 @@
+<?php
+function readPlain($fileIn) {
+    if (file_exists($fileIn)) {
+        $fileContents = file_get_contents($fileIn);
+
+        if ($fileContents !== false) {
+            return $fileContents;
+        } else {
+            echo "ERROR - Failed to read text file: $fileIn";
+            return null;
+        }
+    } else {
+        echo "ERROR - Text file $fileIn not found...";
+        return null;
+    }
+}
+
+$textFile = "";
+$data = readPlain($textFile);
+
+if ($data !== null) {
+    echo $data;
+}
+?>
