16170c0847ccaae7c442b338ca088db716e7fd2a
Andrey <98714688+Dreymeister@users.noreply.github.com>
2023-09-18 16:26:38 -0400

    Added CSV Function

--- lib/readCVS.php
+++ lib/readCVS.php
@@ -1,32 +1,23 @@
+<?php
 function readCSV($fileIn) {
-    if (file_exists($fileIn)) {
-        // Open the CSV file for reading
-        $file = fopen($fileIn, 'r');
-
-        if ($file) {
-            // Read the first row of the CSV file (header row)
-            $header = fgetcsv($file);
-
-            $data = array();
-
-            // Read the remaining rows of the CSV file
-            while (($row = fgetcsv($file)) !== false) {
-                // Combine the header with the current row to create an associative array
-                $data[] = array_combine($header, $row);
-            }
-
-            // Close the CSV file
-            fclose($file);
-
-            return $data;
-        } else {
-            // Handle the case where the file couldn't be opened
-            return "ERROR - Unable to open CSV file: $fileIn";
-        }
-    } else {
-        // Handle the case where the file doesn't exist
+    if (!file_exists($fileIn)) {
         return "ERROR - CSV file $fileIn not found...";
     }
+
+    $file = fopen($fileIn, 'r');
+    if (!$file) {
+        return "ERROR - Unable to open CSV file: $fileIn";
+    }
+
+    $header = fgetcsv($file);
+    $data = [];
+
+    while ($row = fgetcsv($file)) {
+        $data[] = array_combine($header, $row);
+    }
+
+    fclose($file);
+    return $data;
 }
 
 // Example usage:
@@ -37,3 +28,4 @@ if (!is_array($data)) {
 } else {
     print_r($data); // Print the CSV data as an array
 }
+?>
