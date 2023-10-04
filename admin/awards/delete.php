d327b273c6f99675209c9d4e4d663701d25f8ae5
Andrey <98714688+Dreymeister@users.noreply.github.com>
2023-10-04 15:59:00 -0400

    Reformated Scripts

--- admin/awards/delete.php
+++ admin/awards/delete.php
@@ -48,10 +48,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['confirm_delete'])) {
             <a href="index.php" class="btn btn-secondary">Cancel</a>
         </form>
     </div>
-
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <!-- Scipts not working here! -->
+    <script src=""></script>
+    <script src=""></script>
+    <script src=""></script>
 </body>
 
 </html>
