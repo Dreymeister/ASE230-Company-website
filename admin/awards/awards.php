f03349cd1d4a24a00431710b931283eacf9134d8
Dreymeister <drey.owen@gmail.com>
2023-11-13 11:58:04 -0500

    Added utility + delcares

--- admin/awards/awards.php
+++ admin/awards/awards.php
@@ -1,11 +1,72 @@
 <?php
 
+class Awards {
+
+    // Display awards
+    public static function displayAwards($dir_path) {
+        // Get array of awards
+        $awards = self::readAwards($dir_path);
+        $headings = ["Award", "Description"];
+
+        // Create table
+        echo '<div class="container-fluid">';
+        echo '<table class="table" style="width: 90%; margin: auto;">';
+        echo '<thead><tr>';
+        
+        foreach ($headings as $heading) {
+            echo '<th>' . $heading . '</th>';
+        }
+
+        echo '</tr></thead><tbody>';
+
+        foreach ($awards as $award) {
+            $award->printAwardRow();
+        }
+
+        echo '</tbody></table></div>';
+    }
+
+    private static function readAwards($dir_path) {
+        $awardsJson = file_get_contents($dir_path);
+        $awardsData = json_decode($awardsJson, true);
+
+        $awards = [];
+        foreach ($awardsData as $awardData) {
+            $awards[] = new Award($awardData['name'], $awardData['description']);
+        }
+
+        return $awards;
+    }
+}
+
+class Award {
+    private $name;
+    private $description;
+
+    public function __construct($name, $description) {
+        $this->name = $name;
+        $this->description = $description;
+    }
+
+    // Print the award row in the table
+    public function printAwardRow() {
+        echo '<tr>';
+        echo '<td>' . $this->name . '</td>';
+        echo '<td>' . $this->description . '</td>';
+        echo '</tr>';
+    }
+}
+
+// Delete Award
+
 function deleteAward($awardName) {
-    $awards = getAwards();
+    $awards = Awards::getAwards();
     unset($awards[$awardName]);
     $awardsJson = json_encode($awards, JSON_PRETTY_PRINT);
     file_put_contents('../../data/awards.json', $awardsJson);
 }
+
+// Get award
 
 function getAwards() {
     $awardsJson = file_get_contents('../../data/awards.json');
@@ -14,9 +75,11 @@ function getAwards() {
 }
 
 function getAward($awardName) {
-    $awards = getawards();
+    $awards = getAwards();
     return $awards[$awardName];
 }
+
+// Table 
 
 function tableRowAwards($awards) {
     foreach($awards as $award => $details){
@@ -27,6 +90,8 @@ function tableRowAwards($awards) {
             </tr>';
     }
 }
+
+// Update
 
 function updateAwards($post){
     $awards = getAwards();
